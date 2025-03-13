e03e0aa88af7b5a374f5e8801df974d1f0425196
SmartRTE <2232194759@qq.com>
2025-03-13 21:15:18 +0800

    更新到v6.3.0

--- js/CommonFunctions.js
+++ js/CommonFunctions.js
@@ -703,7 +703,7 @@ async function initializeBackgroundList() {
  */
 async function initializeUserCourseDanList() {
 	let l = [
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
 	];
 	let list = $('#user-course-dan-list');
 	l.forEach(function (li) {
